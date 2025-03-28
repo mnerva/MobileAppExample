c962d51229436cf69111f37219aa202e08eea2eb
mnerva <mona-rea.nerva@voco.ee>
2025-03-28 21:30:26 +0200

    Header

---
+++ components/Header/style.ts
@@ -0,0 +1,27 @@
+import { colors } from "../../utils/colors";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 54,
+    marginTop: 10,
+    marginLeft: 10,
+  },
+  Icon: {
+    width: 18,
+    height: 18,
+  },
+  title: {
+    color: colors.blue,
+    fontSize: 26,
+    fontWeight: "500",
+    paddingHorizontal: 16,
+  },
+  space: {
+    width: 24,
+  }
+});
+
+export default styles;
