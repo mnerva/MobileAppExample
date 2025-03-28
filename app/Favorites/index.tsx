8d1c8576619be945281216d302c433fa12849337
mnerva <mona-rea.nerva@voco.ee>
2025-03-28 22:21:27 +0200

    Create favorites screen

--- app/Favorites/index.tsx
+++ app/Favorites/index.tsx
@@ -1,10 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, FlatList } from "react-native";
 import styles from "./style";
+import Header from "../../components/Header"
+import FavoriteItem from "../../components/FavoriteItem";
+import products from "../../utils/products";
 
 export default function Favorites() {
+    const renderItem = ({item}: {item: any}) => {
+      return (
+          <FavoriteItem item={item} />
+      )
+    }
     return (
-        <View>
+      <SafeAreaView>
+        <View style={styles.container}>
+            <Header title="Favorites" showBack={false} showSearch={true} />
             <Text>Favorites</Text>
+            <FlatList data={products} renderItem={renderItem}
+            keyExtractor={(item) => item.id.toString()}
+            />
         </View>
+      </SafeAreaView>
     )
 }
---
+++ components/FavoriteItem/style.ts
@@ -0,0 +1,37 @@
+import { StyleSheet, Dimensions } from "react-native";
+import { colors } from "../../utils/colors";
+
+const {width} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+    container: {
+      marginHorizontal: 24,
+      paddingVertical: 16,
+      flexDirection: 'row',
+      borderBottomWidth: 1,
+      borderBottomColor: colors.borderColor,
+    },
+    title: {
+      color: colors.textGrey,
+      paddingVertical: 8,
+    },
+    price: {
+      color: colors.black,
+      paddingBottom: 8,
+    },
+    image: {
+      width: 100,
+      height: 100,
+      borderRadius: 10,
+      marginRight: 16,
+    },
+    content: {
+      flex: 1,
+    },
+    icon: {
+      width: 24,
+      height: 24,
+    },
+});
+
+export default styles;
