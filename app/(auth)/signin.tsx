6ee31eee219da3f63883f2add5160b35a38b1975
mnerva <mona-rea.nerva@voco.ee>
2025-08-26 19:21:15 +0300

    fixed

--- app/(auth)/signin.tsx
+++ app/(auth)/signin.tsx
@@ -1,29 +1,42 @@
-import { colors } from "@/utils/colour";
-import { StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, SafeAreaView } from "react-native";
+import { useRouter } from "expo-router";
+import styles from "./signinStyle";
+import Header from "@/components/Header";
+import Input from "@/components/Input";
+import Seperator from "@/components/Separator";
+import Button from "@/components/Button";
+import GoogleLogin from "@/components/GoogleLogin";
+import { useAuth } from "@/utils/auth";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 24,
-  },
-  button: {
-    marginVertical: 20,
-  },
-  footerContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 56,
-  },
-  footerText: {
-    color: colors.blue,
-  },
-  footerLink: {
-    color: colors.blue,
-    fontWeight: 'bold',
-    marginLeft: 8,
-  }
-});
+export default function Signin() {
+  const router = useRouter();
+  const { signIn } = useAuth();
 
-export default styles;
+  const handleBackPress = () => {
+    router.back();
+  };
+
+  const handleSignIn = () => {
+    signIn();
+    router.replace('/(app)/(tabs)');
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Header title="Sign In" onBackPress={handleBackPress} />
+        <Input label="Email" placeholder="some@one.com"/>
+        <Input isPassword label="Password" placeholder="******"/>
+        <Button style={styles.button} title="Sign In" onPress={handleSignIn} />
+        <Seperator text='Or sign up with' />
+        <GoogleLogin />
+        <View style={styles.footerContainer}>
+          <Text style={styles.footerText}>Don't have an account?</Text>
+          <TouchableOpacity onPress={() => router.push("/signup")}>
+            <Text style={styles.footerLink}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+} 
